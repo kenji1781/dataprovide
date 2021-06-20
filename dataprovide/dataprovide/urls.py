3dd3d0ffad59f4315a36f6adb0cfd4ed876978f6
KenjiMishima <kenji1781@gmail.com>
2021-06-20 23:55:09 +0900

    アプリ側urls.pyの追加

--- dataprovide/dataprovide/urls.py
+++ dataprovide/dataprovide/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import main_app.views as mainapp
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main_app',mainapp.index),
+    path('main_app/',include('main_app.urls')),
     
 ]
