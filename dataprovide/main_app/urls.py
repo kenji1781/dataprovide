a4636d4c7f73466f64037dde7c6403e13a33a4b5
KenjiMishima <kenji1781@gmail.com>
2021-07-21 05:03:45 +0900

    objects.all追加p168

--- dataprovide/main_app/urls.py
+++ dataprovide/main_app/urls.py
@@ -6,5 +6,5 @@ path('',views.index,name='index'),でプロジェクトurls.py側にて設定さ
 """
 urlpatterns = [
     path('',views.index,name='index'),
-    path('next',views.next,name='next'),
+    #path('next',views.next,name='next'),
 ]
--- dataprovide/main_app/views.py
+++ dataprovide/main_app/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import machine_data
 
 #urls.pyのpathにある関数が実行する
 def index(request):
+    #データベースを全て読み出す。
+    data = machine_data.objects.all()
+
     #htmlファイル側に使われている変数title,msg,gotoの変数を設定
     #paramsに辞書形式で設定し、renderで返す。
     params = {
         'title':'Inamoto Dataprovide',
         'msg':  'アイナックス稲本（株）データープロバイドシステム',
-        'goto': 'next',
+        #'goto': 'next',
+        'data': data,
     }
     #renderの罤２引数にてどのhtmlファイルを読み出すか指定している
     return render(request,'main_app/index.html',params)
