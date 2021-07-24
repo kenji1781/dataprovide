07165801dba85e913dd6c8be087186f5710b3ee7
KenjiMishima <kenji1781@gmail.com>
2021-07-24 09:43:38 +0900

    修正3-13

--- dataprovide/main_app/views.py
+++ dataprovide/main_app/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import machine_data
+from .forms import main_appForm
 
 #urls.pyのpathにある関数が実行する
 def index(request):
-    #データベースを全て読み出す。
-    data = machine_data.objects.all()
+    
 
-    #htmlファイル側に使われている変数title,msg,gotoの変数を設定
+    #htmlファイル側に使われている変数title,msg等の変数を設定
     #paramsに辞書形式で設定し、renderで返す。
     params = {
         'title':'Inamoto Dataprovide',
         'msg':  'アイナックス稲本（株）データープロバイドシステム',
-        #'goto': 'next',
-        'data': data,
+        'form': main_appForm(),
+        'data': [],
     }
+    if (request.method == 'POST'):
+        num = request.POST['id']
+        item = machine_data.objects.get(id=num)
+        params['data'] = [item]
+        params['form'] = main_appForm(request.POST)
+    else:
+        #データベースを全て読み出す。
+        data = machine_data.objects.all()
+
+
     #renderの罤２引数にてどのhtmlファイルを読み出すか指定している
     return render(request,'main_app/index.html',params)
 
