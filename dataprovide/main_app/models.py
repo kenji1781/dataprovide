15554ddfa52aa38f61123294ef8d5f34c3b2112e
KenjiMishima <62679752+kenji1781@users.noreply.github.com>
2021-08-05 14:43:59 +0900

    3-13

--- dataprovide/main_app/models.py
+++ dataprovide/main_app/models.py
@@ -7,6 +7,8 @@ class machine_data(models.Model):
 	machine_name = models.CharField(max_length=100)
 	unit_no = models.IntegerField(default=0)
 	ship_date = models.DateField()
+	drying_time = models.IntegerField(default=0)
+
 	def __str__(self):
 		return '<machine_data:id=' + str(self.id) + ',' + \
 		self.machine_name + '(' + str(self.unit_no) + ')>'
