6571d61db60c1fdac0cf6e1d7af783ea739a95ea
KenjiMishima <62679752+kenji1781@users.noreply.github.com>
2021-08-06 09:41:10 +0900

    全項目追加

--- dataprovide/main_app/models.py
+++ dataprovide/main_app/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 
 class machine_data(models.Model):
-	machine_name = models.CharField(max_length=100)
+	machine_name = models.CharField(max_length=16)
 	unit_no = models.IntegerField(default=0)
-	ship_date = models.DateField()
+	#ship_date = models.DateField()
 	drying_time = models.IntegerField(default=0)
+	run_count = models.IntegerField(default=0)
+	run_time_m = models.IntegerField(default=0)
+	run_time_s = models.IntegerField(default=0)
+	gas_usage = models.IntegerField(default=0)
+	date_m = models.IntegerField(default=0)
+	date_d = models.IntegerField(default=0)
 
 	def __str__(self):
 		return '<machine_data:id=' + str(self.id) + ',' + \
