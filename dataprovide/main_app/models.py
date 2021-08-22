88cef78a958bcfb92d915f76ef8beab365275794
KenjiMishima <kenji1781@gmail.com>
2021-08-22 12:25:26 +0900

    modelsにDateField追加

--- dataprovide/main_app/models.py
+++ dataprovide/main_app/models.py
@@ -8,14 +8,15 @@ class machine_data(models.Model):
 	unit_no = models.IntegerField(default=0)
 	course_no = models.IntegerField(default=0)
 	#ship_date = models.DateField()
-	drying_time = models.IntegerField(default=0)
-	run_count = models.IntegerField(default=0)
-	run_time_m = models.IntegerField(default=0)
-	run_time_s = models.IntegerField(default=0)
-	gas_usage = models.IntegerField(default=0)
-	date_y = models.IntegerField(default=0)
-	date_m = models.IntegerField(default=0)
-	date_d = models.IntegerField(default=0)
+	drying_time = models.IntegerField(default=0,blank=True,null=True)
+	run_count = models.IntegerField(default=0,blank=True,null=True)
+	run_time_m = models.IntegerField(default=0,blank=True,null=True)
+	run_time_s = models.IntegerField(default=0,blank=True,null=True)
+	gas_usage = models.IntegerField(default=0,blank=True,null=True)
+	date_y = models.IntegerField(default=0,blank=True,null=True)
+	date_m = models.IntegerField(default=0,blank=True,null=True)
+	date_d = models.IntegerField(default=0,blank=True,null=True)
+	date_machine = models.DateField(blank=True,null=True)
 
 	def __str__(self):
 		return '<machine_data:id=' + str(self.id) + ',' + \
