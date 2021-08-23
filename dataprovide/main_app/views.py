316291350bc911ec2c0ab9612691628bad213b09
KenjiMishima <62679752+kenji1781@users.noreply.github.com>
2021-08-23 14:17:25 +0900

    DATETIMEに書き込み追加

--- dataprovide/main_app/views.py
+++ dataprovide/main_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import machine_data #データベースモデル
 from .forms import main_appForm #フォーム
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger #ページ制御
+import datetime
 
 def index(request, num=1):
     find = []
@@ -17,20 +18,33 @@ def index(request, num=1):
         #**********フォームcourse**********
         #course = request.POST['course']
         #**********フォームyear検索**********
-        year = request.POST['year']
+        #year = request.POST['year']
         #**********フォームmonth検索**********
-        month = request.POST['month']
+        #month = request.POST['month']
         #**********フォームday検索**********
-        day = request.POST['day']
-        find = machine_data.objects.filter(machine_name=m,unit_no=u,date_y=year,date_m=month,date_d=day)
+        #day = request.POST['day']
+        find = machine_data.objects.filter(machine_name=m,unit_no=u)
+        #find = machine_data.objects.filter(machine_name=m,unit_no=u,date_y=year,date_m=month,date_d=day)
         #**********ページ制御**********
     else:
         form = main_appForm()
         find = machine_data.objects.all().order_by('machine_name','unit_no','-date_y','date_m','date_d')
     paginator = Paginator(find, 7)
+    
 
     try:
         data = paginator.page(num)
+        for i in data:
+            year  = int(i.date_y)
+            month = int(i.date_m)
+            day = int(i.date_d)
+            date = datetime.date(year,month,day)
+            print(year,month,day)
+            print(date)
+            i.date_machine = date
+            i.save() 
+            
+
     except PageNotAnInteger:
         data = paginator.page(1)
     except EmptyPage:
